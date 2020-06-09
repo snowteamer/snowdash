1fc0e78acc6032f5b7fb39c3def738a30403a127
snowteamer <64228468+snowteamer@users.noreply.github.com>
2020-06-09 07:19:31 +0200

    Upload core.js

---
+++ esm/core.js
@@ -0,0 +1,19 @@
+/**
+ * @file core.js - Static utilities for EcmaScript language types.
+ *
+ * @author snowteamer <https://github.com/snowteamer>
+ *
+ * @license AGPL-3.0
+ */
+
+export { default as Arrays } from "./Arrays";
+export { default as Functions } from "./Functions";
+export { default as Iterables } from "./Iterables";
+export { default as Iterators } from "./Iterators";
+export { default as JsonObjects } from "./JsonObjects";
+export { default as Maps } from "./Maps";
+export { default as Numbers } from "./Numbers";
+export { default as Objects } from "./Objects";
+export { default as RegExps } from "./RegExps";
+export { default as Sets } from "./Sets";
+export { default as Strings } from "./Strings";
